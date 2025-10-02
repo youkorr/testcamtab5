e9a356e1231434ce39b4fc3ba6be6df56cbf5721
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-02 02:07:41 +0200

    Create lvgl_camera_display.h

---
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/lvgl/lvgl_esphome.h"
+#include "../tab5_camera/tab5_camera.h"
+
+namespace esphome {
+namespace lvgl_camera_display {
+
+class LVGLCameraDisplay : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  
+  void set_camera(tab5_camera::Tab5Camera *camera) { this->camera_ = camera; }
+  void set_canvas_obj(lv_obj_t *canvas) { this->canvas_obj_ = canvas; }
+  void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
+  
+  float get_setup_priority() const override { return setup_priority::LATE; }
+
+ protected:
+  tab5_camera::Tab5Camera *camera_{nullptr};
+  lv_obj_t *canvas_obj_{nullptr};
+  uint32_t update_interval_{100};  // 100ms = 10 FPS
+  uint32_t last_update_{0};
+  
+  void update_canvas_();
+};
+
+}  // namespace lvgl_camera_display
+}  // namespace esphome
