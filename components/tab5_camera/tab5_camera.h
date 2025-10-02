78fc699c6c714126695f62358e94231dea28d028
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-03 00:47:50 +0200

    Update tab5_camera.h

--- components/tab5_camera/tab5_camera.h
+++ components/tab5_camera/tab5_camera.h
@@ -3,24 +3,11 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "esphome/components/i2c/i2c.h"
-
-// Headers ESP-IDF pour SCCB (I2C caméra)
 #include "esp_sccb_intf.h"
-
-
-// Headers pour le driver caméra (si disponibles)
-#ifdef __has_include("esp_cam_sensor.h")
-#include "esp_cam_sensor.h"
-#endif
-
-#ifdef __has_include("esp_cam_sensor_detect.h")
-#include "esp_cam_sensor_detect.h"
-#endif
 
 namespace esphome {
 namespace tab5_camera {
 
-// Garder vos enums pour la config YAML
 enum CameraResolution {
   RESOLUTION_1080P = 0,
   RESOLUTION_720P = 1,
@@ -50,7 +37,6 @@ class Tab5Camera : public Component, public i2c::I2CDevice {
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  // Configuration
   void set_external_clock_pin(GPIOPin *pin) { this->xclk_pin_ = pin; }
   void set_external_clock_frequency(uint32_t freq) { this->xclk_frequency_ = freq; }
   void set_reset_pin(GPIOPin *pin) { this->reset_pin_ = pin; }
@@ -61,7 +47,6 @@ class Tab5Camera : public Component, public i2c::I2CDevice {
   void set_jpeg_quality(uint8_t quality) { this->jpeg_quality_ = quality; }
   void set_framerate(uint8_t fps) { this->framerate_ = fps; }
 
-  // API publique
   bool capture_frame();
   bool start_streaming();
   bool stop_streaming();
@@ -86,28 +71,16 @@ class Tab5Camera : public Component, public i2c::I2CDevice {
   
   bool initialized_{false};
   bool streaming_{false};
-  bool csi_initialized_{false};
   CameraFrameBuffer frame_buffer_{};
   
-  // Handles officiels
-  esp_cam_sensor_device_t *sensor_device_{nullptr};
   esp_sccb_io_handle_t sccb_handle_{nullptr};
   
-  #ifdef CONFIG_ISP_ENABLED
-  esp_cam_ctlr_handle_t cam_ctlr_handle_{nullptr};
-  #endif
-  
-  // Méthodes privées
   bool init_sensor_with_official_driver_();
-  bool init_csi_interface_();
-  bool capture_csi_frame_();
+  bool init_sc202cs_manually_();
   bool allocate_frame_buffer_();
   void free_frame_buffer_();
   bool start_external_clock_();
-  
-  // Helpers
   void get_resolution_dimensions_(uint16_t &width, uint16_t &height);
-  esp_cam_sensor_output_format_t convert_pixel_format_();
 };
 
 }  // namespace tab5_camera
