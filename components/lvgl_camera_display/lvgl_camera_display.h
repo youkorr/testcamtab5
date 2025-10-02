145ec6b2d3699a55fcfe5d339d2a9f120be7423e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-02 02:33:24 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -14,14 +14,21 @@ class LVGLCameraDisplay : public Component {
   void dump_config() override;
   
   void set_camera(tab5_camera::Tab5Camera *camera) { this->camera_ = camera; }
-  void set_canvas_obj(lv_obj_t *canvas) { this->canvas_obj_ = canvas; }
+  void set_canvas_id(const std::string &canvas_id) { this->canvas_id_ = canvas_id; }
   void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
+  
+  // Méthode pour configurer le canvas après que LVGL soit prêt
+  void configure_canvas(lv_obj_t *canvas) { 
+    this->canvas_obj_ = canvas;
+    ESP_LOGI("lvgl_camera_display", "Canvas configuré: %p", canvas);
+  }
   
   float get_setup_priority() const override { return setup_priority::LATE; }
 
  protected:
   tab5_camera::Tab5Camera *camera_{nullptr};
   lv_obj_t *canvas_obj_{nullptr};
+  std::string canvas_id_{};
   uint32_t update_interval_{100};  // 100ms = 10 FPS
   uint32_t last_update_{0};
   
