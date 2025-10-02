c85894d73e22b7625475aee6ced486493c441d42
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-02 02:07:00 +0200

    Create __init__.py

---
+++ components/lvgl_camera_display/__init__.py
@@ -0,0 +1,41 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+from esphome.components import lvgl
+
+DEPENDENCIES = ["lvgl"]
+AUTO_LOAD = []
+
+CONF_CAMERA_ID = "camera_id"
+CONF_CANVAS_ID = "canvas_id"
+CONF_UPDATE_INTERVAL = "update_interval"
+
+lvgl_camera_display_ns = cg.esphome_ns.namespace("lvgl_camera_display")
+LVGLCameraDisplay = lvgl_camera_display_ns.class_("LVGLCameraDisplay", cg.Component)
+
+# Import du namespace tab5_camera
+tab5_camera_ns = cg.esphome_ns.namespace("tab5_camera")
+Tab5Camera = tab5_camera_ns.class_("Tab5Camera")
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(LVGLCameraDisplay),
+    cv.Required(CONF_CAMERA_ID): cv.use_id(Tab5Camera),
+    cv.Required(CONF_CANVAS_ID): cv.string,  # ID du canvas LVGL
+    cv.Optional(CONF_UPDATE_INTERVAL, default="100ms"): cv.positive_time_period_milliseconds,
+}).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
+    # Lier à la caméra
+    camera = await cg.get_variable(config[CONF_CAMERA_ID])
+    cg.add(var.set_camera(camera))
+    
+    # Définir l'intervalle de mise à jour
+    update_interval_ms = config[CONF_UPDATE_INTERVAL].total_milliseconds
+    cg.add(var.set_update_interval(int(update_interval_ms)))
+    
+    # Note: Le canvas LVGL sera configuré au runtime
+    # via un lambda dans la configuration LVGL
