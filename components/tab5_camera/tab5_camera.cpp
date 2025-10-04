efc1f3da84dc32bcfd88343fdbaac1be6cd35ac3
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-04 14:00:37 +0200

    Update tab5_camera.cpp

--- components/tab5_camera/tab5_camera.cpp
+++ components/tab5_camera/tab5_camera.cpp
@@ -226,7 +226,7 @@ static const sc202cs_reginfo_t init_reglist_1280x720_30fps[] = {
     {SC202CS_REG_END, 0x00},
 };
 
-// Configuration 640x480 RAW8 - CORRIGÉE sans flip/mirror
+// Configuration VGA 640x480 RAW8 - VRAIE résolution VGA avec windowing centré
 static const sc202cs_reginfo_t init_reglist_640x480_30fps[] = {
     {0x0103, 0x01},          {SC202CS_REG_SLEEP_MODE, 0x00},
     {0x36e9, 0x80},          {0x36ea, 0x06},
@@ -285,20 +285,22 @@ static const sc202cs_reginfo_t init_reglist_640x480_30fps[] = {
     {0x3e00, 0x00},          {0x3e01, 0x4d},
     {0x3e02, 0xc0},          {0x3e09, 0x00},
     {0x4509, 0x28},          {0x450d, 0x61},
-    // Configuration directe sans windowing - résolution native
-    {0x3200, 0x00},          {0x3201, 0x00},  // Start X = 0
-    {0x3202, 0x00},          {0x3203, 0x00},  // Start Y = 0  
-    {0x3204, 0x05},          {0x3205, 0x07},  // End X = 1287
-    {0x3206, 0x02},          {0x3207, 0xd7},  // End Y = 727
-    {0x3208, 0x02},          {0x3209, 0x80},  // Width = 640
-    {0x320a, 0x01},          {0x320b, 0xe0},  // Height = 480
-    {0x3210, 0x00},          {0x3211, 0x04},  // X offset = 4
-    {0x3212, 0x00},          {0x3213, 0x02},  // Y offset = 2
+    // Windowing VGA 640x480 centré depuis résolution max 1280x720
+    // Start X = (1280-640)/2 = 320 = 0x140
+    // Start Y = (720-480)/2 = 120 = 0x78
+    {0x3200, 0x01},          {0x3201, 0x40},  // Start X = 0x0140 (320)
+    {0x3202, 0x00},          {0x3203, 0x78},  // Start Y = 0x0078 (120)
+    {0x3204, 0x03},          {0x3205, 0xbf},  // End X = 320+640-1 = 959 = 0x03BF
+    {0x3206, 0x02},          {0x3207, 0x57},  // End Y = 120+480-1 = 599 = 0x0257
+    {0x3208, 0x02},          {0x3209, 0x80},  // Width = 640 = 0x0280
+    {0x320a, 0x01},          {0x320b, 0xe0},  // Height = 480 = 0x01E0
+    {0x3210, 0x00},          {0x3211, 0x00},  // X offset = 0
+    {0x3212, 0x00},          {0x3213, 0x00},  // Y offset = 0
     // VTS/HTS pour 30fps
-    {0x320c, 0x07},          {0x320d, 0x80},  // HTS = 1920
-    {0x320e, 0x04},          {0x320f, 0xe2},  // VTS = 1250
-    // PAS de flip/mirror - orientation normale (0x3221 = 0x00)
-    {0x3221, 0x00},  // Pas de binning, pas de flip/mirror
+    {0x320c, 0x05},          {0x320d, 0x00},  // HTS = 1280
+    {0x320e, 0x02},          {0x320f, 0xd0},  // VTS = 720
+    // PAS de flip/mirror - orientation normale
+    {0x3221, 0x00},
     {SC202CS_REG_END, 0x00},
 };
 
