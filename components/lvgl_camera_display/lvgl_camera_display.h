d81ea80b0e3de9f2287c34cf6b842119d7731043
youkorr <96953496+youkorr@users.noreply.github.com>
2025-10-05 16:56:45 +0200

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -12,29 +12,37 @@ class LVGLCameraDisplay : public Component {
   void setup() override;
   void loop() override;
   void dump_config() override;
-  
+
   void set_camera(tab5_camera::Tab5Camera *camera) { this->camera_ = camera; }
   void set_canvas_id(const std::string &canvas_id) { this->canvas_id_ = canvas_id; }
+
+  // Permet de régler l'intervalle d'update, utile pour passer à 30 FPS
   void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
-  
+
   void configure_canvas(lv_obj_t *canvas);
-  
+
   float get_setup_priority() const override { return setup_priority::LATE; }
 
  protected:
   tab5_camera::Tab5Camera *camera_{nullptr};
   lv_obj_t *canvas_obj_{nullptr};
   std::string canvas_id_{};
-  uint32_t update_interval_{100};  // 100ms = 10 FPS
+
+  // Intervalle pour mise à jour (en ms) ; 33 ms pour ~30 FPS
+  uint32_t update_interval_{33};  // 33ms = 30 FPS
   uint32_t last_update_{0};
-  
-  // Variables pour contrôle des logs
+
+  // Variables pour contrôle des logs et mesure FPS réel
   uint32_t frame_count_{0};
   bool first_update_{true};
   bool canvas_warning_shown_{false};
-  
+
+  // Variable pour calcul du FPS réel
+  uint32_t last_fps_time_{0};
+
   void update_canvas_();
 };
 
 }  // namespace lvgl_camera_display
 }  // namespace esphome
+
